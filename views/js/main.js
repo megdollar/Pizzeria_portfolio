8cba21e627b8b0a2c4ded004d675a0a26e326205
Megan Dollar <megdollar@gmail.com>
2016-12-12 13:02:07 -0500

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -523,7 +523,7 @@ function updatePositions() {
     var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
     logAverageFrame(timesToUpdatePosition);
   }
-  requestAnimationFrame(updatePostions);
+  requestAnimationFrame(updatePositions);
 }
 
 // runs updatePositions on scroll
